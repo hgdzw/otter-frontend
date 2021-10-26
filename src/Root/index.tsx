0157caf61fc5c9f388472c15a034d8f802d2a37e
Otter King <>
2021-10-26 16:21:35 +0800

    feat: hide app domain

--- src/Root/index.tsx
+++ src/Root/index.tsx
@@ -5,7 +5,7 @@ import { HashRouter } from 'react-router-dom';
 
 function Root() {
   const isApp = (): boolean => {
-    return window.location.host.includes('app');
+    return window.location.host.includes('app') && window.location.host !== 'app.otterclam.finance'; // TODO: remove this after launch
   };
 
   const app = () => (
