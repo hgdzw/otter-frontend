b7a5933ff3ee2a1bf4ebc889415700e3dfbc007f
Siferbel <info@siferbel.com>
2021-10-31 04:50:46 +0100

    Initial commit ido page setup

--- src/views/IDO/index.tsx
+++ src/views/IDO/index.tsx
@@ -3,26 +3,62 @@ import { Button, Link } from '@material-ui/core';
 import LandingHeader from 'src/components/LandingHeader';
 import { useWeb3Context } from 'src/hooks';
 import styles from './ido.module.scss';
+import happyOtter from '../IDO/images/otter_happy.png';
+import polygon from '../IDO/images/polygon.svg';
+import { DiscordLink, TwitterLink, MediumLink, GithubLink } from '../../constants';
+import Footer from '../Landing/components/Footer';
+import Countdown from '../Landing/components/Countdown';
 
 export default function IDO() {
   const dispatch = useDispatch();
   const { address, connect, connected, provider, chainID } = useWeb3Context();
   return (
-    <div className={styles.body}>
-      <LandingHeader />
-      <h1 className={styles.title}>Claim your CLAM</h1>
-      <p className={styles.desc}>
-        The IDO will be held from Nov 1, 2021 0:00 UTC to Nov 2, 2021 23:59 UTC. Join the Otter`s Kingdom now!🦦
-      </p>
-      {connected ? (
-        <div>Connected</div>
-      ) : (
-        <div className={styles.button}>
-          <Button variant="contained" color="primary" size="large" disableElevation onClick={connect}>
-            Connect Your Wallet
-          </Button>
+    <div>
+      <div className={styles.body}>
+        <LandingHeader />
+        <h1 className={styles.title}>Claim your CLAM</h1>
+        <p className={styles.desc}>
+          The IDO will be held from Nov 1, 2021 0:00 UTC to Nov 2, 2021 23:59 UTC. Join the Otter`s Kingdom now!🦦
+        </p>
+
+        <div className={styles.mainBox}>
+          <div className={styles.happyOtterBox}>
+            <img src={happyOtter} className={styles.happyOtter} />
+          </div>
+
+          <div className={styles.currentMaiBox}>
+            <img src={polygon} className={styles.polygon} />
+            <h1>Current Amount (MAI)</h1>
+            <h2>$520,121.00</h2>
+          </div>
         </div>
-      )}
+
+        <div className={styles.claimClamBox}>
+          <p className={styles.title}>
+            Claim your <span className={styles.highlight}>CLAM</span> to join the Otter Kingdom now!
+          </p>
+          <div className={styles.web3Box}>
+            {connected ? (
+              <div>Connected</div>
+            ) : (
+              <div className={styles.button}>
+                <Button variant="contained" color="primary" size="large" disableElevation onClick={connect}>
+                  Connect Your Wallet
+                </Button>
+              </div>
+            )}
+          </div>
+          <p className={styles.learnMore}>
+            <a href={MediumLink}>Learn more</a>
+          </p>
+        </div>
+
+        <div className={styles.countdownBox}>
+          <p className={styles.title}>Pre-sale Countdown</p>
+          <Countdown />
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 }
--- src/views/Landing/components/Countdown/index.tsx
+++ src/views/Landing/components/Countdown/index.tsx
@@ -3,7 +3,40 @@ import styles from './countdown.module.scss';
 export default function Countdown() {
   return (
     <div className={styles.main}>
-      <h2 className={styles.title}>Coming Soon</h2>
+      <div className={styles.days}>
+        <div className={styles.day}>
+          <p id="day-1">0</p>
+        </div>
+        <div className={styles.day}>
+          <p id="day-2">0</p>
+        </div>
+      </div>
+
+      <div className={styles.hours}>
+        <div className={styles.hour}>
+          <p id="hour-1">0</p>
+        </div>
+        <div className={styles.hour}>
+          <p id="hour-2">0</p>
+        </div>
+      </div>
+      <div className={styles.minutes}>
+        <div className={styles.minute}>
+          <p id="minute-1">0</p>
+        </div>
+        <div className={styles.minute}>
+          <p id="minute-2">0</p>
+        </div>
+      </div>
+
+      <div className={styles.seconds}>
+        <div className={styles.second}>
+          <p id="second-1">0</p>
+        </div>
+        <div className={styles.second}>
+          <p id="second-2">0</p>
+        </div>
+      </div>
     </div>
   );
 }
